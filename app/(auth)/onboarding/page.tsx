4ea0fd80dce00c9c57b756059b7aa3d0d4e88e02
rhandyta <rhandytabriantama@gmail.com>
2023-12-05 21:01:41 +0700

    onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,29 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
 async function Page() {
+    const user = await currentUser();
+    const userInfo = {};
+    const userData = {
+        id: user?.id,
+        objectId: userInfo?._id,
+        username: userInfo?.username || user?.username,
+        name: userInfo?.name || user?.firstName || "",
+        image: userInfo?.image || user?.imageUrl,
+        bio: userInfo?.bio || user?.bio || ""
+    }
+
     return (
-        <main>
-            <h1 className="head-text">Onboarding</h1>
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+            <h3 className="head-text">Onboarding</h3>
+            <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use Threads.</p>
+
+            <section className="mt-9 bg-dark-2 p-10">
+                <AccountProfile 
+                    user={userData}
+                    btnTitle="Continue"
+                />
+            </section>
         </main>
     )
 }
